a611db0279f44bdad3bcda373c11028d5a16007a
Daniel Fuertes <d.fcoiras@upm.es>
2024-05-22 18:17:50 +0200

    Small bugfix

--- replicate_results.sh
+++ replicate_results.sh
@@ -1,8 +1,20 @@
 # 20 nodes
-python test.py data/nop/2depots/const/20/scenario_seed0_0-20obs.pkl data/nop/2depots/const/20/scenario_seed0_5-20obs.pkl data/nop/2depots/const/20/scenario_seed0_10-20obs.pkl data/nop/2depots/const/20/scenario_seed0_15-20obs.pkl data/nop/2depots/const/20/scenario_seed0_20-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max20/scenario_seed1_0-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max20/scenario_seed1_5-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max20/scenario_seed1_10-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max20/scenario_seed1_15-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max20/scenario_seed1_20-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
 
 # 50 nodes
-python test.py data/nop/2depots/const/50/scenario_seed0_0-20obs.pkl data/nop/2depots/const/50/scenario_seed0_5-20obs.pkl data/nop/2depots/const/50/scenario_seed0_10-20obs.pkl data/nop/2depots/const/50/scenario_seed0_15-20obs.pkl data/nop/2depots/const/50/scenario_seed0_20-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max50/scenario_seed1_0-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max50/scenario_seed1_5-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max50/scenario_seed1_10-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max50/scenario_seed1_15-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max50/scenario_seed1_20-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
 
 # 100 nodes
-python test.py data/nop/2depots/const/100/scenario_seed0_0-20obs.pkl data/nop/2depots/const/100/scenario_seed0_5-20obs.pkl data/nop/2depots/const/100/scenario_seed0_10-20obs.pkl data/nop/2depots/const/100/scenario_seed0_15-20obs.pkl data/nop/2depots/const/100/scenario_seed0_20-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max100/scenario_seed1_0-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max100/scenario_seed1_5-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max100/scenario_seed1_10-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max100/scenario_seed1_15-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
+python test.py data/nop/2depots/const/max100/scenario_seed1_20-20obs.pkl --model pretrained/comparison/naviformer --use_cuda false
